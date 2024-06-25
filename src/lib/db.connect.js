5736d7c6e0eca1cc6c89458e758174580a4404be
imabuzerkhan <abuzerk41@gmail.com>
2024-06-25 18:00:37 +0545

    comment

--- src/lib/db.connect.js
+++ src/lib/db.connect.js
@@ -1,23 +1,27 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'; // mongoose library ko import kar rahe hain
 
-const connection = {};
+const connection = {}; // ek empty object banaya connection ke liye
 
-async function dbconnect(){
-  if(connection.isconnected){
-    console.log("already connect")
-    return
+async function dbConnect() {
+  // Database ke saath connection check kar rahe hain ya dekh rahe hain ki connection already establish hai
+  if (connection.isConnected) {
+    console.log('Already connected to the database'); // Agar pehle se connected hai to message print hoga
+    return; // aur function return kar jayega
   }
-  try{
-    const db = await mongoose.connect(process.env.MONGODB_URI || " " , {})
 
-    connection.isConnected = db.connections[0].readyState;
+  try {
+    // Database ke saath connection establish karne ki koshish kar rahe hain
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {}); // MongoDB URI se database connect kar rahe hain
 
-    console.log('Database connected successfully');
-  } catch(error){
-    console.error('Database connection failed:', error);
+    connection.isConnected = db.connections[0].readyState; // Connection ka status update kar rahe hain
 
-    // Graceful exit in case of a connection error
-    process.exit(1);
+    console.log('Database connected successfully'); // Agar connection successful ho jaye to message print hoga
+  } catch (error) {
+    console.error('Database connection failed:', error); // Agar connection fail ho jaye to error message print hoga
+
+    // Connection error ke case me process ko exit kar rahe hain
+    process.exit(1); // Error ke case me process ko terminate kar dete hain
   }
 }
-export default dbconnect
+
+export default dbConnect; // dbConnect function ko export kar rahe hain
